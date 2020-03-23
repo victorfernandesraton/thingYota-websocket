ebd8ed7861adb16842a8b51fbda146e67f5e61ba
victorfernandesraton <vfbraton@gmail.com>
2020-03-23 01:58:07 -0300

    update for ws

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const
   server = require("./server"),
   socketio = require("socket.io"),
   routes = require("./routes/"),
-  io = socketio.listen(server.server);
+  io = socketio(server.server);
 
 
 const
@@ -22,16 +22,30 @@ require('dns').lookup(require('os').hostname(), (err, add, fam) => {
 io.sockets.on('connection', (socket, req) => {
   console.info(new Date)
   console.log(`Connection : ${socket.id}`)
+  socket.emit("teste","hi")
   // chat nessage
   socket.on('message', payload => {
     chat(payload, io.sockets);
   });
+
+  socket.on('teste', payload => {
+    console.log(payload)
+  })
+
+  socket.on('ok', payload => {
+
+    console.log("ok")
+  })
 
   // arduino
   socket.on("arduino", payload => {
     console.log("payload");
   })
 });
+
+io.sockets.on("ok", payload => {
+  console.log("ok")
+})
 
 
 // Hook de rotas
